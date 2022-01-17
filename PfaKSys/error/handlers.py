ea3c40a0cf3d5f649142fca5d5197ac6f99abe06
Nuhser <martinimmel@gmail.com>
2022-01-17 17:06:56 +0100

    Change text to message keys

--- PfaKSys/error/handlers.py
+++ PfaKSys/error/handlers.py
@@ -1,4 +1,5 @@
 from flask import current_app, Blueprint, render_template
+from flask_babel import gettext
 
 
 error_blueprint = Blueprint('error', __name__)
@@ -6,14 +7,14 @@ error_blueprint = Blueprint('error', __name__)
 
 @error_blueprint.app_errorhandler(403)
 def error_403(error):
-    return render_template('errors/403.html', debug_mode=current_app.debug, error=error), 403
+    return render_template('errors/403.html', title=gettext('page.error_403.title'), debug_mode=current_app.debug, error=error), 403
 
 
 @error_blueprint.app_errorhandler(404)
 def error_404(error):
-    return render_template('errors/404.html', debug_mode=current_app.debug, error=error), 404
+    return render_template('errors/404.html', title=gettext('page.error_404.title'), debug_mode=current_app.debug, error=error), 404
 
 
 @error_blueprint.app_errorhandler(500)
 def error_500(error):
-    return render_template('errors/500.html', debug_mode=current_app.debug, error=error), 500
+    return render_template('errors/500.html', title=gettext('page.error_500.title'), debug_mode=current_app.debug, error=error), 500
