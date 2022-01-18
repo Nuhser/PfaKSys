0ed05d42b3c892652b6e5f547d68341b1b7d64f8
Nuhser <martinimmel@gmail.com>
2022-01-18 17:07:09 +0100

    Add new error handler

--- PfaKSys/error/handlers.py
+++ PfaKSys/error/handlers.py
@@ -15,6 +15,11 @@ def error_404(error):
     return render_template('errors/404.html', title=gettext('page.error_404.title'), debug_mode=current_app.debug, error=error), 404
 
 
+@error_blueprint.app_errorhandler(405)
+def error_405(error):
+    return render_template('errors/405.html', title=gettext('page.error_405.title'), debug_mode=current_app.debug, error=error), 405
+
+
 @error_blueprint.app_errorhandler(500)
 def error_500(error):
     return render_template('errors/500.html', title=gettext('page.error_500.title'), debug_mode=current_app.debug, error=error), 500
