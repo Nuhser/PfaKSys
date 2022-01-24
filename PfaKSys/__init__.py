f326ecf1f0ca03dce84124e1c2135243e63242de
Nuhser <martinimmel@gmail.com>
2022-01-24 10:47:07 +0100

    Add constraint naming scheme

--- PfaKSys/__init__.py
+++ PfaKSys/__init__.py
@@ -7,16 +7,27 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 from PfaKSys.config.config import Config, init_logging
 
 
 init_logging()
 
+# set SQL naming conventions
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+
 # initialize flask extensions
 babel = Babel()
 bcrypt = Bcrypt()
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=metadata)
 login_manager = LoginManager()
 mail = Mail()
 migrate = Migrate()
