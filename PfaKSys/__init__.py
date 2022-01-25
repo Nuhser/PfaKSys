9c7765415b6d583ed43144337318f9ffeb875f12
Nuhser <martinimmel@gmail.com>
2022-01-25 12:16:31 +0100

    Remove default user group

--- PfaKSys/__init__.py
+++ PfaKSys/__init__.py
@@ -64,7 +64,6 @@ def create_app(config=Config):
         from PfaKSys.models import Item, ItemCategory, ItemLocation, User, UserGroup, UserSettings
         with app.app_context():
             db.create_all()
-            db.session.add(UserGroup(name='default'))
             db.session.add(UserGroup(name='admin'))
             db.session.commit()
 
